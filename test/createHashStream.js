ae1331e2d7979fd4c8c59df81a3485b97b18d99b
Vytautas Jakutis <vytautas@jakut.is>
2015-12-05 16:05:45 +0200

    test/createHashStream: use random values

--- test/createHashStream.js
+++ test/createHashStream.js
@@ -7,31 +7,41 @@ var through2 = require('through2');
 describe('createHashStream', function() {
   it('works', function() {
     return Promise
-      .bind({})
+      .bind({
+        buffer: new Buffer('beep' + Math.random()),
+        realAlgorithm: 'sha512',
+        algorithm: 'abc' + Math.random(),
+      })
       .then(function setup() {
-        this.stream = through2();
-        this.stream.end('beepboop\n');
+        var self = this;
 
-        var hash = crypto.createHash('sha512');
-        hash.update(new Buffer('beepboop\n'));
+        this.stream = through2();
+        this.stream.end(this.buffer);
+
+        var hash = crypto.createHash(this.realAlgorithm);
+        hash.update(this.buffer);
         this.hash = hash.digest();
 
         this.boundary = new lib.Boundary();
         var createHash = this.boundary.createHash;
         this.createHashSpy = stub(this.boundary, 'createHash', function() {
           // Execute contract A.
-          return createHash.call(this, 'sha512');
+          return createHash.call(this, self.realAlgorithm);
         });
-        this.hashStream = lib.createHashStream(this.boundary, 'abcdef');
       })
       .then(function exercise() {
+        this.hashStream = lib.createHashStream(this.boundary, this.algorithm);
+      })
+      .then(function verify(result) {
         return lib.pumpAndConcat(this.boundary, [this.stream, this.hashStream]);
       })
       .then(function verify(result) {
         expect(result.toString('hex')).to.equal(this.hash.toString('hex'));
+
         // Check collaboration A.
         expect(this.createHashSpy).to.have.been.calledOnce;
-        expect(this.createHashSpy).to.have.been.calledWithExactly('abcdef');
+        expect(this.createHashSpy)
+          .to.have.been.calledWithExactly(this.algorithm);
       });
   });
 });
