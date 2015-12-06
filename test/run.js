e49e4cf33081ae35ee262aaa71cd44d87d28fa17
Vytautas Jakutis <vytautas@jakut.is>
2015-12-06 10:31:24 +0200

    test/run: try out grouping contracts and collaborations

--- test/run.js
+++ test/run.js
@@ -4,6 +4,32 @@ var lib = require('..');
 var crypto = require('crypto');
 var through2 = require('through2');
 
+var points = {
+  _pending: [],
+  _collaborations: [],
+  contract: function(names, fn) {
+    this._pending = this._pending.concat(names);
+    fn();
+  },
+  collaboration: function(names, fn) {
+    this._collaborations.push(function() {
+      this._pending = _.difference(this._pending, [].concat(names));
+      return fn();
+    }.bind(this));
+  },
+  verify: function() {
+    var self = this;
+    return Promise
+      .map(self._collaborations, function(collaboration) {
+        return collaboration();
+      })
+      .then(function() {
+        expect(self._pending).to.eql([]);
+      });
+  },
+};
+_.bindAll(points);
+
 describe('run', function() {
   it('hashes a single file in directory', function() {
     return Promise
@@ -16,23 +42,51 @@ describe('run', function() {
       .then(function setup() {
         var self = this;
 
-        // Fake contract C.
-        this.boundary.argv = [this.directory];
-        // Fake contract D.
-        this.boundary.stdout = through2();
-
-        var fs = this.boundary.fs;
-
-        this.readDirectorySpy = stub(fs, 'readDirectory', function() {
-          // Fake contract A.
-          return Promise.resolve(['.', '..', self.filename]);
+        points.contract('D', function() {
+          self.boundary.stdout = through2();
         });
 
-        this.createReadStreamSpy = stub(fs, 'createReadStream', function() {
-          // Fake contract B.
-          var stream = through2();
-          stream.end(self.buffer);
-          return stream;
+        points.collaboration('D', function() {
+          var output = lib.pumpAndConcat(self.boundary, [self.boundary.stdout]);
+          return Promise
+            .resolve(output)
+            .then(function(output) {
+              self.output = output.toString('utf8');
+            });
+        });
+
+        points.contract('C', function() {
+          self.boundary.argv = [self.directory];
+        });
+
+        points.contract('A', function() {
+          var fs = self.boundary.fs;
+          self.readDirectorySpy = stub(fs, 'readDirectory', function() {
+            return Promise.resolve(['.', '..', self.filename]);
+          });
+        });
+
+        points.collaboration(['A', 'C'], function() {
+          expect(self.readDirectorySpy).to.have.been.calledOnce;
+          expect(self.readDirectorySpy)
+            .to.have.been.calledWithExactly(self.directory);
+        });
+
+        points.contract('B', function() {
+          var fs = self.boundary.fs;
+          self.createReadStreamSpy = stub(fs, 'createReadStream', function() {
+            var stream = through2();
+            stream.end(self.buffer);
+            return stream;
+          });
+        });
+
+        points.collaboration(['B', 'C'], function() {
+          var path = self.directory + '/' + self.filename;
+
+          expect(self.createReadStreamSpy).to.have.been.calledOnce;
+          expect(self.createReadStreamSpy)
+            .to.have.been.calledWithExactly(path);
         });
 
         var shasum = crypto.createHash('sha512');
@@ -42,23 +96,9 @@ describe('run', function() {
       .then(function exercise() {
         return lib.run(this.boundary);
       })
+      .then(points.verify)
       .then(function verify() {
-        return lib.pumpAndConcat(this.boundary, [this.boundary.stdout]);
-      })
-      .then(function verify(output) {
-        // Check collaboration D.
-        expect(output.toString('utf8'))
-          .to.equal(this.hash + ' /' + this.filename + '\n');
-
-        // Check collaborations B and C.
-        expect(this.createReadStreamSpy).to.have.been.calledOnce;
-        expect(this.createReadStreamSpy)
-          .to.have.been.calledWithExactly(this.directory + '/' + this.filename);
-
-        // Check collaborations A and C.
-        expect(this.readDirectorySpy).to.have.been.calledOnce;
-        expect(this.readDirectorySpy)
-          .to.have.been.calledWithExactly(this.directory);
+        expect(this.output).to.equal(this.hash + ' /' + this.filename + '\n');
       });
   });
 });
